155b0dfa005a6465033ad0e4e86a01a74a6128d8
Роман <roman@MacBook-Air-Irina.local>
2022-07-30 11:42:36 +0300

    some changes

---
+++ src/util.rs
@@ -0,0 +1,35 @@
+use bevy::{reflect::Reflect, math::Rect, prelude::default};
+
+pub trait Lerp<T> {
+    fn lerp(self, other: T, t: f32) -> T;
+}
+
+
+impl Lerp<f32> for f32 {
+    fn lerp(self, other: f32, t: f32) -> f32 {
+        self * (1. - t) + other * t
+    }
+}
+
+
+pub trait RectExtensions<T: Reflect + PartialEq> {
+    fn horizontal(value: T) -> Self;
+    fn top(value: T) -> Self;
+}
+
+impl<T: Reflect + PartialEq + Default + Clone> RectExtensions<T> for Rect<T> {
+    fn horizontal(value: T) -> Self {
+        Self {
+            left: value.clone(),
+            right: value,
+            ..default()
+        }
+    }
+
+    fn top(value: T) -> Self {
+        Self {
+            top: value,
+            ..default()
+        }
+    }
+}
