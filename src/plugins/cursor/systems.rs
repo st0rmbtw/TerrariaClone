98f5fa6a001ab05626c7c23e23ebe84c14decbca
roman <carpowroma@gmail.com>
2023-02-27 23:10:10 +0300

    Remove redundant casts

--- src/plugins/cursor/systems.rs
+++ src/plugins/cursor/systems.rs
@@ -179,20 +179,19 @@ pub fn update_cursor_position(
                     style.position.top = Val::Px(wnd.height() - screen_pos.y);
                 }
 
-                let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+                let window_size = Vec2::new(wnd.width(), wnd.height());
 
                 // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
                 let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
 
                 // matrix for undoing the projection and camera transform
-                let ndc_to_world =
-                    camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+                let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
 
                 // use it to convert ndc to world-space coordinates
-                let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+                let world_pos = ndc_to_world.project_point3(ndc.extend(1.0));
 
                 // reduce it to a 2D value
-                let world_pos: Vec2 = world_pos.truncate();
+                let world_pos = world_pos.truncate();
 
                 cursor.position = screen_pos;
                 cursor.world_position = world_pos;
